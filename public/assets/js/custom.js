10a9cb60d03d92afbf80f317e346174ed9258928
marcoking13 <marcokhodr16@gmail.com>
2022-04-03 01:51:11 -0700

    api key update

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -95,11 +95,11 @@ window.onload = function() {
 //---------------------------------------------------------------------------------------------------------------------------------------------//
 //------------------------------------------------------YouTube variables--------------------------------------------------------------------------//
   var tubeURL = "https://www.googleapis.com/youtube/v3/";
-  var youTubeKey = "AIzaSyC4tz1TDHpgGTkAyNR9ycjU0cixA6bDNnk";
+  var youTubeKey = "AIzaSyBOIJTAHXfZ3GKM8Sj5QCMNYBeOMsJX8_I";
 
   var videoSearch = '';
 //----------------------------------------------------------YouTube API-------------------------------------------------------------------------------------//
-  let getYouTube = function(){
+  let getYouTube = function(){ 
     videoSearch = tubeURL + "search?&q=" + topic + '%20tutorial' + "&part=snippet&chart=mostPopular&videoCategoryId=27&type=video&relevanceLanguage=en&maxResults=1&key=" + youTubeKey;
     var youtubeId = $('#' + topic + 'video');
 
@@ -109,8 +109,9 @@ window.onload = function() {
       dataType: 'jsonp'
     })
     .done(function(response) {
-      var videoId = response.items[0].id.videoId;
-      youtubeId.append("<iframe width='100%' height='100%' src='https://www.youtube.com/embed/" + videoId + "' frameborder='0'id='hi'></iframe>")
+      console.log(response);
+      // var videoId = response.items[0].id.videoId;
+      // youtubeId.append("<iframe width='100%' height='100%' src='https://www.youtube.com/embed/" + videoId + "' frameborder='0'id='hi'></iframe>")
     });
 
 
